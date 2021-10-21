94c1c5fb1cfe3239124f45ee9ee74de02abd9f68
Jenny Han <jlhan@stanford.edu>
2021-10-21 15:36:58 -0700

    Update client.js

--- public/client.js
+++ public/client.js
@@ -44,6 +44,39 @@ var grammarSelect = document.querySelector("#grammarForm");
 grammarSelect.addEventListener("input", grammarSelected);
 
 const button = document.getElementById("submit");
+let utterance;
+
+function say(text) {
+    utterance = new SpeechSynthesisUtterance(text);
+    utterance.voice = speechSynthesis.getVoices()[33];
+    speechSynthesis.speak(utterance);
+}
+
+button.addEventListener("click", function(e) {
+  speechSynthesis.cancel(utterance);
+  fetch("/clicked", {
+    method: "POST",
+    body: JSON.stringify({
+      grammarName: selected,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+    .then(function(response) {
+      return response.text().then(function(text) {
+        console.log("Click was recorded");
+        document.getElementById("sentence").innerHTML = text;
+        document.getElementById("sentencedisplay").style.display = "block";
+        say(text);
+      });
+      
+      throw new Error("Request failed.");
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+});
 
 button.addEventListener("click", function (e) {
   fetch("/clicked", {
