2381f925b2d34d2f1de6b16d34e8176075a756b7
jenny han <jlhan@stanford.edu>
2022-01-30 21:55:33 -0800

    reset

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 var bodyParser = require("body-parser");
-const port = 8000;
+const port = 8080;
 app.use(express.static("public"));
 app.use(bodyParser.json());
 
