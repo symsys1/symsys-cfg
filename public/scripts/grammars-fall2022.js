d74a1c5769452a14a9a084ade6f0fadca8b0a14e
jenny han <jlhan@stanford.edu>
2022-10-21 01:25:42 -0400

    add fall 2022

---
+++ public/scripts/grammars-fall2022.js
@@ -0,0 +1,486 @@
+var grammars = [
+  {
+    name: "2. THE SPARROW BOT (Abena Ofosu)",
+    fileName: "./grammars/Abena.js",
+  },
+  {
+    name: "3. Stanford Administration Mail (Lee Rosenthal and Abigail Neely)",
+    fileName: "./grammars/AbigailLee.js",
+  },
+  {
+    name: "4. GPS Directions (Agustin Olvera)",
+    fileName: "./grammars/Agustin.js",
+  },
+  {
+    name: "5. Stanford Student Asking Peer Out (Ahmad A., George D., Jennifer F.)",
+    fileName: "./grammars/Ahmad, George, Jen.js",
+  },
+  {
+    name: "6. The Stanford Daily Pitchbot ( AJ Nadel, Bhakti Anbarasan, Michelle Bao)",
+    fileName: "./grammars/AJ_Bhakti_Michelle.js",
+  },
+  {
+    name: "7. Stoner Speech (Dean Alamy, Akshana Dassanaike )",
+    fileName: "./grammars/Akshana & Dean.js",
+  },
+  {
+    name: "8. Frat Bro Simulator (Alex Le, Joseph Tey)",
+    fileName: "./grammars/Alex Le and Joseph Tey.js",
+  },
+  {
+    name: "9. Provost Persis Drell (Alexa Kupor)",
+    fileName: "./grammars/AlexaKupor.js",
+  },
+  {
+    name: "10. Nick Cage in National Treasure Grammar (Alexander Karr)",
+    fileName: "./grammars/AlexanderK.js",
+  },
+  {
+    name: "11. Based Bateman Bot (Alex Agris, Austin Kendall, Zeina Hashem)",
+    fileName: "./grammars/AlexAustinZeina.js",
+  },
+  {
+    name: "12. Frat Bro Working the Door (Alexander Chon, Lucien Roose)",
+    fileName: "./grammars/AlexLucien.js",
+  },
+  {
+    name: "13. Golf Commentators (Ali Ansari)",
+    fileName: "./grammars/Ali Grammar.js",
+  },
+  {
+    name: "14. Doggos (Alice)",
+    fileName: "./grammars/AliceZ.js",
+  },
+  {
+    name: "15. Sports Commentator (Ali Hindy, WIll Denton, Devon Smith)",
+    fileName: "./grammars/AliDevonWill.js",
+  },
+  {
+    name: "16. Minecraft Youtuber (Allen Wu)",
+    fileName: "./grammars/AllenW.js",
+  },
+  {
+    name: "17. Stanford Dorm Chats (Ananya V., Sidharth S., Pooja A.)",
+    fileName: "./grammars/Ananya, Sidharth, Pooja.js",
+  },
+  {
+    name: "18. Mean Teacher (Anavi Baddepudi, Karina Chen, Katherine Wong)",
+    fileName: "./grammars/AnaviKarinaKatherine.js",
+  },
+  {
+    name: "19. Overheard at Gates (Andrew Lee, Carlo Dino)",
+    fileName: "./grammars/AndrewCarlo.js",
+  },
+  {
+    name: "20. FIFA commentator (Andrew Wu, Yahaya Ndutu)",
+    fileName: "./grammars/AndrewYahaya.js",
+  },
+  {
+    name: "21. Excuses for Canceled Plans (Angelina Ye, Alicia Purpur)",
+    fileName: "./grammars/Angelina, Alicia.js",
+  },
+  {
+    name: "22. Dr. House M.D. (Angelique Charles-Davis)",
+    fileName: "./grammars/Angelique.js",
+  },
+  {
+    name: "23. Student-athlete CFG (Annabelle B., Iris K., Lettie C.)",
+    fileName: "./grammars/Annabelle_Iris_Lettie.js",
+  },
+  {
+    name: "24. Shakespearean Insults (Anna Goldman)",
+    fileName: "./grammars/AnnaG.js",
+  },
+  {
+    name: "25. Love Hypothesis: Stanford Romance Novel (Anna Zheng)",
+    fileName: "./grammars/AnnaZ.js",
+  },
+  {
+    name: "26. Dear Professor (Patricia Strutz, Ariana Lotfi)",
+    fileName: "./grammars/Ariana_Patricia.js",
+  },
+  {
+    name: "27. Person in Classroom (Aryan Sahai)",
+    fileName: "./grammars/Aryan.js",
+  },
+  {
+    name: "28. ProFro at Stanford Admit Weekend (Aya Aziz, Akosua Kissi)",
+    fileName: "./grammars/Aya Aziz, Akosua Kissi.js",
+  },
+  {
+    name: "29. IYKYK (Beatriz Stix-Brunell)",
+    fileName: "./grammars/Beatriz Stix-Brunell Grammar.js",
+  },
+  {
+    name: "30. Symsys Discussion Board (Jayson G., Natalie W., Belinda Y.)",
+    fileName: "./grammars/belindajaysonnatalie.js",
+  },
+  {
+    name: "31. Country Singer (Brandon Bui, Lishan Carroll)",
+    fileName: "./grammars/Brandon and Lishan.js",
+  },
+  {
+    name: "32. Bad Comedian (Brandon Rupp, Akshar Sarvesh)",
+    fileName: "./grammars/Brandon_and_Akshar.js",
+  },
+  {
+    name: "33. LinkedIn Post (Kaelyn Ong, Brandon Lieu)",
+    fileName: "./grammars/BrandonLieu_KaelynOng.js",
+  },
+  {
+    name: "34. Frosh GroupMe (Caitlynn Tran and Jocelyn Tran)",
+    fileName: "./grammars/CaitlynnJocelyn.js",
+  },
+  {
+    name: "35. Stanford Friends (Caroline C., Alex L., Noa B.)",
+    fileName: "./grammars/caroline_alex_noa.js",
+  },
+  {
+    name: "36. NSO (Carter Sun, Aditi Tuli, Chi Pham)",
+    fileName: "./grammars/CarterChiAditi.js",
+  },
+  {
+    name: "37. The Computer Therapist (Cassady Hill)",
+    fileName: "./grammars/Cassady.js",
+  },
+  {
+    name: "38. Diner Talk (Cass Kramer)",
+    fileName: "./grammars/CassK.js",
+  },
+  {
+    name: "39. Spotify Wrapped (Eli Waldman, Catherine Harbour)",
+    fileName: "./grammars/Catherine_Eli.js",
+  },
+  {
+    name: "40. NFT advertiser (Emanuel Adamiak, Chase Adams)",
+    fileName: "./grammars/Chase_Emanuel.js",
+  },
+  {
+    name: "41. Better Than D.A.R.E. (Lucas B. & Daniel K.)",
+    fileName: "./grammars/DanielLucas.js",
+  },
+  {
+    name: "42. Canadian, eh? (Danny Goldberg, Willow Herz)",
+    fileName: "./grammars/Danny Goldberg & Willow Herz.js",
+  },
+  {
+    name: "43. PWR Students (Daphne Liu, Devanshu Ladsaria)",
+    fileName: "./grammars/DaphneDevanshu.js",
+  },
+  {
+    name: "44. InstaGirl Comments (Divya M., Teddy S., Ishita G.)",
+    fileName: "./grammars/DivyaTeddyIshita.js",
+  },
+  {
+    name: "45. Dwight Schrute III (Isaac Krementsov)",
+    fileName: "./grammars/DwightS.js",
+  },
+  {
+    name: "46. VolleyBros (Dylan Pichay)",
+    fileName: "./grammars/DylanPichay.js",
+  },
+  {
+    name: "47. Michael Scott Unfiltered (Edwin A., Jose R.)",
+    fileName: "./grammars/Edwin Amaya & Jose Ruiz.js",
+  },
+  {
+    name: "48. LinkedIn Update  (Eleanor Peng)",
+    fileName: "./grammars/EleanorPeng.js",
+  },
+  {
+    name: "49. Soto's common phrases (Eli Wandless)",
+    fileName: "./grammars/Eli Wandless (2ND VERSION).js",
+  },
+  {
+    name: "50. Tonald Drump (Elliot Dauber)",
+    fileName: "./grammars/ElliotDauber.js",
+  },
+  {
+    name: "51. Gollum/Smeagol (Emilio Melgoza)",
+    fileName: "./grammars/Emilio 2022-01-28 12.52.55.js",
+  },
+  {
+    name: "52. Gen z internet behavior  (Emma (Kexin) Wang, Ayanna M.)",
+    fileName: "./grammars/EmmaAyanna.js",
+  },
+  {
+    name: "53. New Yawker (Eric Zhu, Jenna Kim, Akea Pavel)",
+    fileName: "./grammars/Eric Jenna Akea.js",
+  },
+  {
+    name: "54. Ethan Dormski (Ethan Bernheim, Eric Montes, Natalie S.)",
+    fileName: "./grammars/ethan_eric_natalie.js",
+  },
+  {
+    name: "55. Big Game Disaster  (Eva Ye, Carl Zhang)",
+    fileName: "./grammars/Eva&Carl.js",
+  },
+  {
+    name: "56. Gamer Chat (Felicity Huang)",
+    fileName: "./grammars/FelicityHuangGrammar.js",
+  },
+  {
+    name: "57. Sadness CFG (Alexandra Takei, Diletta Milana, Hagar Gal)",
+    fileName: "./grammars/HagarDilettaAlexandra.js",
+  },
+  {
+    name: "58. Stanford Marriage Pact  (Kailiti S., Madison Y., Hannah Y.)",
+    fileName: "./grammars/HannahKalitiMadison.js",
+  },
+  {
+    name: "59. Selina Meyer from Veep (Henry Shen)",
+    fileName: "./grammars/henry.js",
+  },
+  {
+    name: "60. Grandma Gams Grammar  (Daniel M., Teddy H., Hlumelo N.)",
+    fileName: "./grammars/HlumeloDannyTeddy.js",
+  },
+  {
+    name: "61. The Hulk (Cyrus D'Arcy)",
+    fileName: "./grammars/Hulk.js",
+  },
+  {
+    name: "62. 1984 Grammar (Ian Lasic-Ellis, Nick Keubler )",
+    fileName: "./grammars/Ian and Nick.js",
+  },
+  {
+    name: "62. King Bach (Ian Miller)",
+    fileName: "./grammars/IanM.js",
+  },
+  {
+    name: "63. Acting Coach (Jaden Southern)",
+    fileName: "./grammars/Jaden.js",
+  },
+  {
+    name: "64. Ujamaa Lingo  (Jalen Hunter Karetina Tessema )",
+    fileName: "./grammars/JalenKaretina.js",
+  },
+  {
+    name: "65. Disney Adult (Janene Kim, Maya Chandra)",
+    fileName: "./grammars/JaneneMaya.js",
+  },
+  {
+    name: "66. Overheard on Love Island (Jasmine Vonk, Emily Liang)",
+    fileName: "./grammars/jasmine_emily.js",
+  },
+  {
+    name: "67. COVID reaches Stanford (Jason Kaul & Ethan Buck)",
+    fileName: "./grammars/Jason-Ethan.js",
+  },
+  {
+    name: "68. Amateur Wine Taster (Jay Yu, Grace Li, Yixuan Xu)",
+    fileName: "./grammars/JayGraceClair2.js",
+  },
+  {
+    name: "69. Stanford Email Headline Generator (Jeewoo C., Thalia R., Brett S.)",
+    fileName: "./grammars/JeewooTahliaBrett.js",
+  },
+  {
+    name: "70. Ultimate Frisbee CFG (Jerred Smith)",
+    fileName: "./grammars/JerredCFG.js",
+  },
+  {
+    name: "71. Stanford Entrepreneurs (JJ Hennessy)",
+    fileName: "./grammars/jj.js",
+  },
+  {
+    name: "72. Yo Mama jokes (Jordyn Case & Mackenzie Chapman)",
+    fileName: "./grammars/JordynMackenzie.js",
+  },
+  {
+    name: "73. Gossip Girl (Julia Lasiota , Yasser Jamal)",
+    fileName: "./grammars/JuliaYasser.js",
+  },
+  {
+    name: "74. Chipotle Orders (Jiwon Choi, June Lee, Alex Paek)",
+    fileName: "./grammars/JuneJiwonAlex.js",
+  },
+  {
+    name: "75. Standup Comedian One/Two-Liners (Kassadey Delgado)",
+    fileName: "./grammars/Kassadey.js",
+  },
+  {
+    name: "76. Stanford Buzz/Fizz App (Kate C., Taylor G., and Gabi P.)",
+    fileName: "./grammars/katetaylorgabi.js",
+  },
+  {
+    name: "77. Dog (Katie Mishra)",
+    fileName: "./grammars/Katie.js",
+  },
+  {
+    name: "78. Edna Mode (Alisa Aleksanian, Kevin Chen, David Castro)",
+    fileName: "./grammars/KevinAlisaDavid.js",
+  },
+  {
+    name: "79. POV: Southie Resident (Kieran B., Roya A., Imgard B.)",
+    fileName: "./grammars/Kieran_Roya_Imgard.js",
+  },
+  {
+    name: "80. Academic-Interest Club Promotional Emails (Larissa Lauer)",
+    fileName: "./grammars/Larissa.js",
+  },
+  {
+    name: "81. MTL (Campbell Jenkins, Larsen Weigle)",
+    fileName: "./grammars/LarsenCampbell.js",
+  },
+  {
+    name: "82. Christian Instagram Bios (Laura Gequelin)",
+    fileName: "./grammars/LauraG2.js",
+  },
+  {
+    name: "83. Stanford Scam Emails (Lauren Lowe)",
+    fileName: "./grammars/Lauren Lowe.js",
+  },
+  {
+    name: "84. COVID Announcement Bot (Laya I., Marvin G., Illiana G.)",
+    fileName: "./grammars/LayaMarvinIlliana.js",
+  },
+  {
+    name: "85. Lear's Nonsense Poems (Sobhana)",
+    fileName: "./grammars/Lear's Nonsense Poems.js",
+  },
+  {
+    name: "86. SLE Kid (Elizabeth Zhu, Lian Wang)",
+    fileName: "./grammars/LianElizabeth.js",
+  },
+  {
+    name: "87. Persis COVID Email (Damanpreet S., Jawad J., Linda L.)",
+    fileName: "./grammars/Linda, Jawad, Damanpreet.js",
+  },
+  {
+    name: "88. LSJUMB (Steven G. Opferman)",
+    fileName: "./grammars/LSJUMB (Steven Opferman).js",
+  },
+  {
+    name: "89. Frat Bro (Mario Nicolas, Ryan Chelugut )",
+    fileName: "./grammars/MarioandRyan.js",
+  },
+  {
+    name: "90. Howdy Partner (Jocelyn Quintero, Missy Fuentes Delgado)",
+    fileName: "./grammars/MissyJocelyn.js",
+  },
+  {
+    name: "91. Nervous Nelly Bot (Morgan Foster and Elli Deligianni)",
+    fileName: "./grammars/MorganElli.js",
+  },
+  {
+    name: "92. Articulating Needs and Boundaries (Natasha Johnson)",
+    fileName: "./grammars/Natasha.js",
+  },
+  {
+    name: "93. Tiger Mom (Nathan Tan, Jeremy Tian, Abhinav Sinha)",
+    fileName: "./grammars/NathanJeremyAbhinav.js",
+  },
+  {
+    name: "94. Bullying The Weird Kid (Noor Hassan)",
+    fileName: "./grammars/NoorH.js",
+  },
+  {
+    name: "95. Heartbroken gurl (Norah Asemota, Diego Maglione, Sontee Scott)",
+    fileName: "./grammars/NorahDiegoSontee.js",
+  },
+  {
+    name: "96. Gordan Ramsey  (Reid Mccaw, Norri Dupree, Malisha Lutchmeea)",
+    fileName: "./grammars/Norri, Reid, Malisha.js",
+  },
+  {
+    name: "97. Type A Stanford Start-Up Student (Nainoa V., Oriana J., and Shaye S.)",
+    fileName: "./grammars/OrianaJacksonNainoaVisperasShayeStory.js",
+  },
+  {
+    name: "98. Down Bad (Rachel Wu, Noah Wong, Sebastian Russo)",
+    fileName: "./grammars/Rachel_Sebastian_Noah.js",
+  },
+  {
+    name: "99. Ranch Talk (Calico)",
+    fileName: "./grammars/ranch_talk_CalicoDucheneaux.js",
+  },
+  {
+    name: "100. Completely reasonable COVID-19 policy generator (Sofia V., Richard Y.)",
+    fileName: "./grammars/Richard, Sofia.js",
+  },
+  {
+    name: "101. Romantic Arthurian (Joy Kim)",
+    fileName: "./grammars/RomanticArthurian.js",
+  },
+  {
+    name: "102. Trump (Ryan Loo, Eduardo Garcia-Escobar)",
+    fileName: "./grammars/RyanEduardo.js",
+  },
+  {
+    name: "103. RA Groupme Messages (Sade Oyekenu and Dominic Terrones:)",
+    fileName: "./grammars/SadeAndDominic.js",
+  },
+  {
+    name: "104. Pick-me person (Samuel Kwok, Silvana Alfaro)",
+    fileName: "./grammars/SamSilvana.js",
+  },
+  {
+    name: "105. Stanford Student Email (Savahna P., Mohamed E., Emilio R.)",
+    fileName: "./grammars/SavahnaMohamedEmilio.js",
+  },
+  {
+    name: "106. Creed Bratton's Creed Thoughts (The Office) (Sejal Moosapeta)",
+    fileName: "./grammars/Sejal2.js",
+  },
+  {
+    name: "107. Anonymously Horny Straight Man (Soliman Aniss)",
+    fileName: "./grammars/Soliman.js",
+  },
+  {
+    name: "108. Yo mama (Solmih Kim)",
+    fileName: "./grammars/SolmihK.js",
+  },
+  {
+    name: "109. Conspiracy Theory Gennerator (Srihari Nageswaran)",
+    fileName: "./grammars/Srihari.js",
+  },
+  {
+    name: "110. StanfordAds (Jessica Chudnovsky, Steven Le)",
+    fileName: "./grammars/Steven Le & Jessica Chudnovsky.js",
+  },
+  {
+    name: "111. Let's Ask Siri (Steven Pu, Brittany Ozioma Linus, Thanh Nhut Tieu)",
+    fileName: "./grammars/Steven, Brittany, Thanh.js",
+  },
+  {
+    name: "112. Stanford Fizz Thoughts (Suzannah Wistreich)",
+    fileName: "./grammars/Suzannah.js",
+  },
+  {
+    name: "113. Carnist Contentions (Tom)",
+    fileName: "./grammars/TomR.js",
+  },
+  {
+    name: "114. Startup Culture (Truc Cody Ho)",
+    fileName: "./grammars/Truc.js",
+  },
+  {
+    name: "115. Gamer Rage (Ulises Medina)",
+    fileName: "./grammars/Ulises Medina.js",
+  },
+  {
+    name: "116. Twitterspeak (Deven Bansal)",
+    fileName: "./grammars/bansal.js",
+  },
+  {
+    name: "117. Gay Vernacular 101 (Faria, Manny, Isa, Paloma)",
+    fileName: "./grammars/gayvernacular.js",
+  },
+  {
+    name: "118. The Grind Never Stops (Joseph Z.)",
+    fileName: "./grammars/JosephZ.js",
+  },
+  {
+    name: "119. Weaboo (Camilla P.)",
+    fileName: "./grammars/CamillaP.js",
+  },
+];
+
+let grammarForm = document.getElementById("grammarForm");
+grammars.forEach((o) => {
+  let opt = document.createElement("option");
+  opt.value = o.fileName;
+  opt.textContent = o.name;
+  grammarForm.appendChild(opt);
+});
